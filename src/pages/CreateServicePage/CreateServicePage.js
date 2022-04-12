3d8d8db18a2f2d18964ed2f74036ded60a3d10e2
Lis Ribeiro <lisribeiro197@gmail.com>
2022-04-12 15:10:47 -0300

    Parte Lis

---
+++ src/pages/CreateServicePage/CreateServicePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class CreateServicePage extends React.Component {
+    render() {
+        return(
+            <div>
+                Create Service Page
+            </div>
+        )
+    }
+}
+
+export default CreateServicePage
